0330b2c5e76c3388355ccb49b5c6881bc2e59b18
Nikita Kamble <nikita.kamble@prorigosoftware.com>
2023-02-20 19:09:26 +0530

    Committing 1 program

---
+++ Assignment_1to150Question/src/Assignment_1to150Question/Question57.java
@@ -0,0 +1,32 @@
+package Assignment_1to150Question;
+
+import java.util.Scanner;
+
+public class Question57 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        System.out.print("Input an integer: ");
+        int n = in.nextInt();
+ 		System.out.println("String format of the said integer: " + transform_int_to_string(n));		
+
+	}
+	public static String transform_int_to_string(int n) {
+	    boolean is_negative = false;
+	    StringBuilder tsb = new StringBuilder();
+	    if (n == 0) {
+	      return "0";
+	    } else if (n < 0) {
+	      is_negative = true;
+	    }
+	    n = Math.abs(n);
+	    while (n > 0) {
+	      tsb.append(n % 10);
+	      n /= 10;
+	    }
+	    if (is_negative) {
+	      tsb.append("-");
+	    }
+	    return tsb.reverse().toString();
+	  }
+}
