97b06d308fa8bd1cd198e88e0f3ad7b434b2fac6
Nikita Kamble <nikita.kamble@prorigosoftware.com>
2023-02-20 19:05:48 +0530

    Commiting 5 programs

---
+++ Assignment_1to150Question/src/Assignment_1to150Question/Question54.java
@@ -0,0 +1,10 @@
+package Assignment_1to150Question;
+
+public class Question54 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Assignment_1to150Question/src/Assignment_1to150Question/question56.java
@@ -0,0 +1,35 @@
+package Assignment_1to150Question;
+
+import java.util.Arrays;
+
+public class question56 {
+
+	public static void main(String[] args) {
+		int[] nums = {-2,3,4,-1,-3,1,2,-4,0};
+		  System.out.println("\nOriginal array: " + Arrays.toString(nums));
+		  int[] result = split_sorting_array(nums);
+		  System.out.println("\nResult: " + Arrays.toString(result));
+
+	}
+	public static int[] split_sorting_array(int[] nums) {
+		  if (nums == null) {
+		   throw new IllegalArgumentException("Null array......!");
+		  }
+		  boolean flag = true;
+		  while (flag) {
+		   flag = false;
+		   for (int j = 0; j < nums.length - 1; j++) {
+		    if (nums[j] > nums[j + 1]) {
+		     swap(nums, j, j + 1);
+		     flag = true;
+		    }
+		   }
+		  }
+		  return nums;
+		 }
+		 private static void swap(int[] nums, int left, int right) {
+		  int temp = nums[right];
+		  nums[right] = nums[left];
+		  nums[left] = temp;
+		 }
+}
